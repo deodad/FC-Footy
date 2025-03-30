855bda33a939906a68d8c4275c2b9ad4230bbc1c
Tony D'Addeo <amdaddeo@gmail.com>
2025-03-30 11:34:56 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,40 @@ import { Metadata } from "next";
 import App from "./app";
 import { Providers } from "./providers";
 
-const appUrl = process.env.NEXT_PUBLIC_URL;
-
-const frame = {
-  version: "next",
-  imageUrl: `${appUrl}/opengraph-image`,
-  button: {
-    title: "FC Footy App",
-    action: {
-      type: "launch_frame",
-      name: "FC Footy App",
-      url: appUrl,
-      splashImageUrl: `${appUrl}/defifa_spinner.gif`,
-      splashBackgroundColor: "#010513",
-    },
-  },
-};
+const appUrl = process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000';
 
 export const revalidate = 300;
 
-export async function generateMetadata(): Promise<Metadata> {
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
+  const url = new URL('/', appUrl);
+  
+  Object.entries(await searchParams).forEach(([key, value]) => {
+    if (typeof value === 'string') {
+      url.searchParams.append(key, value);
+    }
+  });
+  
+  const frame = {
+    version: "next",
+    imageUrl: `${appUrl}/opengraph-image`,
+    button: {
+      title: "FC Footy App",
+      action: {
+        type: "launch_frame",
+        name: "FC Footy App",
+        url: url.href,
+        splashImageUrl: `${appUrl}/defifa_spinner.gif`,
+        splashBackgroundColor: "#010513",
+      },
+    },
+  };
+
+  console.log(frame);
+  
   return {
     title: "FC Footy App",
     openGraph: {
